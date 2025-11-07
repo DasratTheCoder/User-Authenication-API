f7e207100d0cf675c2eddd1004a367a64d0a85ae
dasrat12 <dasratkumar16official@gmail.com>
2025-11-07 11:45:27 +0500

    Add Cors And Cookie-Parser

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 import dotenv from 'dotenv'
+import { app } from './app.js'
+const PORT = process.env.PORT || 3000
 import { connect_DB } from './db/index.js'
-dotenv.config({path:"./.env"})
-connect_DB()
+dotenv.config({ path: "./.env" })
+connect_DB().then(
+    app.listen(PORT, () => {
+        console.log(`The app is listening on PORT ${PORT}`)
+    })
+).catch((err) => {
+    console.log(`The MongoDB has not been connected successfully`)
+    process.exit(1)
+})
