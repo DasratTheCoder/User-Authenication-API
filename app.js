0c6c00f4f5ca0ba58fe6e982731d72ca25720ef0
dasrat12 <dasratkumar16official@gmail.com>
2025-11-07 11:52:26 +0500

    Add Basic Route Just For Test

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(cookieParser())
 
+//Routes Import
+import { router } from './routes/user.route.js'
+
+//Secured Routes
+app.use("/api/v1/user",router)
 
 export {
     app
