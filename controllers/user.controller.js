e93ccfe7b10a58e5a6575e95ceaa0f25a4a39951
dasrat12 <dasratkumar16official@gmail.com>
2025-11-08 20:19:05 +0500

    Add Login Controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,7 +2,20 @@ import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from '../models/user.model.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
-
+const generatingAccessAndRefreshTokens = async (userId) => {
+    try {
+        const user = await User.findById(userId);
+        if (!user) { throw new ApiError(400, "User Not Found") }
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave: false })
+        return { accessToken, refreshToken }
+    }
+    catch (err) {
+        throw new ApiError(500, "Something Went Wrong While Generating Access And Refresh Tokens")
+    }
+}
 const registerUser = asyncHandler(async (req, res) => {
     const { email, username, fullName, password } = req.body
     // console.log(email)
@@ -31,7 +44,36 @@ const registerUser = asyncHandler(async (req, res) => {
 
 
 
-})
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    if ([email, password].some((field) => field?.trim() === "")) {
+        throw new ApiError(400, "All Fields Are Required")
+    }
+
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new ApiError(400, "User not found")
+    }
+
+    const isPasswordCorrect = await user.isPasswordCorrect(password)
+
+    if (!isPasswordCorrect) {
+        throw new ApiError(400, "Invalid Credentials")
+    }
+
+    const userId = user._id
+
+    const { refreshToken, accessToken } = await generatingAccessAndRefreshTokens(userId)
+
+    return res.status(200).json(
+        new ApiResponse(200, { accessToken, refreshToken }, "You are loged In!")
+    )
+});
 export {
-    registerUser
+    registerUser,
+    loginUser
 }
