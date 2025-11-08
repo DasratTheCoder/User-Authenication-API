a7a8a7663a1ae9234304cc26aedd961ada632e3b
dasrat12 <dasratkumar16official@gmail.com>
2025-11-08 20:29:34 +0500

    add logout endpoint

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -73,7 +73,29 @@ const loginUser = asyncHandler(async (req, res) => {
         new ApiResponse(200, { accessToken, refreshToken }, "You are loged In!")
     )
 });
+const LogOutUser = asyncHandler(async (req, res) => {
+ await   User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: { refreshToken: undefined }
+        },
+        {
+            new: true
+
+        }
+    )
+    const options = {
+        httpsOnly: true,
+        secure: true
+    }
+    return res.status(200).clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(
+            new ApiResponse(200, {}, "User Loged Out Successfully")
+        )
+});
 export {
     registerUser,
-    loginUser
+    loginUser,
+    LogOutUser
 }
